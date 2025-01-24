d822e16922eb577cd6718b27c96817d89a950b36
sjana7797 <sjana7797@gmail.com>
2025-01-24 23:28:45 +0530

    fix: styles

--- apps/admin/src/pages/category/index.tsx
+++ apps/admin/src/pages/category/index.tsx
@@ -9,7 +9,6 @@ import { DataTable } from "@/admin/components/global/data-table";
 import PageTitle from "@/admin/components/global/title";
 import { useGetAllCategories } from "@/admin/hooks/api/category";
 import { Link } from "@tanstack/react-router";
-import { api } from "@/admin/trpc/react";
 
 function CategoryPage() {
   // hooks
@@ -29,8 +28,6 @@ function CategoryPage() {
     })) ?? [];
 
   const nextCursor = data?.nextCursor;
-
-  api.category.getAllCategories.useQuery();
 
   // side effect
   useEffect(() => {
